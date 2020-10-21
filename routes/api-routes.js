d401767a2f0b5163f474ee2d40ee72481084dcfc
Jacob Torres <simplyjacob00@gmail.com>
2020-10-21 18:10:13 -0400

    refined api routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,46 +1,46 @@
-const Workout = requestuire("../models/workoutLogic");
+const Workout = requestuire("../models/workoutScript");
 module.exports = function (app) {
-  app.get("/api/workouts", (request, response) => {
+  app.get("/api/workouts", (req, res) => {
     Workout.find({})
-      .then((responseult) => {
-        response.json(responseult);
+      .then((response) => {
+        res.json(response);
       })
       .catch((err) => {
-        response.status(400).json(err);
+        res.status(400).json(err);
       });
   });
-  app.post("/api/workouts", ({ body }, response) => {
+  app.post("/api/workouts", ({ body }, res) => {
     console.log(body);
     Workout.create(body)
-      .then((responseult) => {
-        response.json(responseult);
+      .then((response) => {
+        res.json(response);
       })
       .catch((err) => {
-        response.status(400).json(err);
+        res.status(400).json(err);
       });
   });
-  app.put("/api/workouts/:id", ({ params, body }, response) => {
+  app.put("/api/workouts/:id", ({ params, body }, res) => {
     Workout.findByIdAndUpdate(
       { _id: params.id },
       { $push: { exercises: [body] } },
       { $inc: { totalDuration: body.duration } }
     )
       .then(() => {
-        Workout.findOne({ _id: params.id }).then((responseult) => {
-          response.json(responseult);
+        Workout.findOne({ _id: params.id }).then((response) => {
+          res.json(response);
         });
       })
       .catch((err) => {
-        response.status(400).json(err);
+        res.status(400).json(err);
       });
   });
-  app.get("/api/workouts/range", (request, response) => {
+  app.get("/api/workouts/range", (req, res) => {
     Workout.find({})
-      .then((responseult) => {
-        response.json(responseult);
+      .then((response) => {
+        res.json(response);
       })
       .catch((err) => {
-        response.status(400).json(err);
+        res.status(400).json(err);
       });
   });
 };
